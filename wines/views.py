25cb93dee4aa90f12f7ffc966d638cdcd5dca132
Alan Isaacs <xtrasenz@gmail.com>
2020-09-25 15:20:35 -0700

    Specify if venv is in use

--- wines/views.py
+++ wines/views.py
@@ -2,12 +2,14 @@
 
 """Bare Flask App"""
 
+import sys
 from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Wine Catalog goes here"
+    p = sys.prefix
+    return "<p>Wine Catalog goes here.</p> <p>Prefix = %s</p>" % (p)
 
 if __name__ == '__main__':
     app.run()
