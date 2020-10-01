b10d5106b1df0b0c7ae4f83167f039d158cf819b
Alan Isaacs <xtrasenz@gmail.com>
2020-10-01 16:55:42 -0700

    Add views for new entries

--- wines/views.py
+++ wines/views.py
@@ -6,7 +6,9 @@ import sys
 from flask import (Flask,
                    render_template,
                    session as login_session,
-                   url_for)
+                   url_for,
+                   request,
+                   redirect)
 from sqlalchemy import (create_engine,
                         asc)
 from sqlalchemy.orm import sessionmaker
@@ -26,7 +28,7 @@ DBSession = sessionmaker(bind=engine)
 
 
 # Display home web page
-@app.route("/")
+@app.route('/')
 def showHome():
     """Display home page"""
     session = DBSession()
@@ -42,6 +44,72 @@ def showHome():
                            wines=wines)
 
 
+# Create a new country
+@app.route('/country/new', methods=['GET', 'POST'])
+def newCountry():
+    """Create a new country"""
+    if request.method == 'POST':
+        newCountry = Country(name=request.form['name'])
+        session = DBSession()
+        session.add(newCountry)
+        session.commit()
+        session.close()
+        return redirect(url_for('showHome'))
+    else:
+        return render_template('newCountry.html')
+
+
+# Create a new region
+@app.route('/region/new', methods=['GET', 'POST'])
+def newRegion():
+    """Create a new region"""
+    session = DBSession()
+    countries = session.query(Country).order_by(asc(Country.name))
+    if request.method == 'POST':
+        newRegion = Region(
+            country_id=request.form['country_id'],
+            name=request.form['name'])
+        session.add(newRegion)
+        session.commit()
+        session.close()
+        return redirect(url_for('showHome'))
+    else:
+        session.close()
+        return render_template('newRegion.html',
+                               countries=countries)
+
+
+# Add a wine to the database
+@app.route('/wine/new', methods=['GET', 'POST'])
+def newWine():
+    """Create a new wine"""
+    session = DBSession()
+    countries = session.query(Country).order_by(asc(Country.name))
+    regions = session.query(Region).order_by(asc(Region.name))
+    if request.method == 'POST':
+        newWine = Wine(
+            country_id=request.form['country_id'],
+            region_id=request.form['region_id'],
+            name=request.form['name'],
+            year=request.form['year'],
+            price=request.form['price'],
+            rating=request.form['rating'],
+            abv=request.form['abv'],
+            date_tasted=request.form['date_tasted'],
+            label_photo=request.form['label_photo'],
+            description=request.form['description'],
+            sommelier_id=request.form['sommelier_id']
+            )
+        session.add(newWine)
+        session.commit()
+        session.close()
+        return redirect(url_for('showHome'))
+    else:
+        session.close()
+        return render_template('newWine.html', countries=countries, regions=regions)
+
+
 if __name__ == '__main__':
+    app.secret_key = 'dev_key'
     app.debug = True
     app.run()
