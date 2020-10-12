be1b8608d8c2a101de350b9d0921c8043be16e05
Alan Isaacs <xtrasenz@gmail.com>
2020-10-12 16:14:11 -0700

    Fix for db hang with new lines

--- wines/views.py
+++ wines/views.py
@@ -32,15 +32,26 @@ DBSession = sessionmaker(bind=engine)
 def showHome():
     """Display home page"""
     session = DBSession()
-    countries = session.query(Country).order_by(asc(Country.name))
-    regions = session.query(Region).order_by(asc(Region.name))
-    sommeliers = session.query(Sommelier).order_by(asc(Sommelier.username))
-    wines = session.query(Wine)
+    # countries = session.query(Country).order_by(asc(Country.name)).all()
+    # regions = session.query(Region).order_by(asc(Region.name)).all()
+    # sommeliers = session.query(Sommelier).order_by(asc(Sommelier.username)).all()
+    wines = session.query(Wine).all()
     session.close()
+    # Replace new line (\r\n) characters in description with <br>
+    # print("======= DEBUG DESCRIPTION =======")
+    for wine in wines:
+        s = (wine.description).replace("\r\n", "<br>")
+        # print("s = %s" % s)
+        # c = [ letter for letter in s ]
+        # print("c = %s" % c)
+        wine.description = s
+        # print("done replacing")
+
+            
     return render_template('home.html',
-                           countries=countries,
-                           regions=regions,
-                           sommeliers=sommeliers,
+                        #    countries=countries,
+                        #    regions=regions,
+                        #    sommeliers=sommeliers,
                            wines=wines)
 
 
@@ -49,12 +60,37 @@ def showHome():
 def showWineTable():
     """Display wine table"""
     session = DBSession()
-    wines = session.query(Wine)
-    countries = session.query(Country).order_by(asc(Country.name))
-    regions = session.query(Region).order_by(asc(Region.name))
+    wines = session.query(Wine).all()
+    # countries = session.query(Country).order_by(asc(Country.name))
+    # regions = session.query(Region).order_by(asc(Region.name))
+    # sommeliers = session.query(Sommelier).order_by(asc(Sommelier.username))
     session.close()
     return render_template('wineTable.html',
                           wines=wines)
+                        #   countries=countries,
+                        #   regions=regions,
+                        #   sommeliers=sommeliers)
+
+
+# Display a single wine for editing
+@app.route('/editWine', methods=['GET', 'POST'])
+def editWine():
+    """Edit a single wine"""
+    if request.method == 'POST':
+        print("======= IN POST ======= ")
+        print("REQUEST.WINEID = %s" % request.form['wineid'])
+        session = DBSession()
+        wineToEdit = session.query(Wine).filter(Wine.id == request.form['wineid']).one_or_none()
+        session.close()
+        # can't iterate through single wine in template
+        # so copying into a dictionary
+        wineDict = {}
+        wineDict = (vars(wineToEdit))
+        # print(wineDict)
+        return render_template('editWine.html', wineToEdit=wineDict)
+    else:
+        print("======= IN GET ======= ")
+        return render_template('editWine.html')
 
 
 # Create and edit both countries and regions
@@ -62,8 +98,8 @@ def showWineTable():
 def manageCountryRegion():
     """Create and edit both countries and regions"""
     session = DBSession()
-    countries = session.query(Country).order_by(asc(Country.name))
-    regions = session.query(Region).order_by(asc(Region.name))
+    countries = session.query(Country).order_by(asc(Country.name)).all()
+    regions = session.query(Region).order_by(asc(Region.name)).all()
     deletableCountries = session.query(Country).join(Region, full = True).filter(Region.country_id == None).all()
     deletableRegions = session.query(Region).join(Wine, full = True).filter(Wine.region_id == None).all()
     if request.method == 'POST':
@@ -73,6 +109,7 @@ def manageCountryRegion():
         session.close()
         return redirect(url_for('manageCountryRegion'))
     else:
+        session.close()
         return render_template('manageCountryRegion.html',
                            countries=countries,
                            regions=regions,
@@ -100,7 +137,7 @@ def newCountry():
 def newRegion():
     """Create a new region"""
     session = DBSession()
-    countries = session.query(Country).order_by(asc(Country.name))
+    countries = session.query(Country).order_by(asc(Country.name)).all()
     if request.method == 'POST':
         newRegion = Region(
             country_id=request.form['country_id'],
@@ -120,8 +157,8 @@ def newRegion():
 def newWine():
     """Create a new wine"""
     session = DBSession()
-    countries = session.query(Country).order_by(asc(Country.name))
-    regions = session.query(Region).order_by(asc(Region.name))
+    countries = session.query(Country).order_by(asc(Country.name)).all()
+    regions = session.query(Region).order_by(asc(Region.name)).all()
     # Replace any empty strings in the form fields with <None> 
     # Note: <None> is inserted into the db as NULL
     # Note: <None> will display in html as the string "None"
@@ -134,7 +171,6 @@ def newWine():
                 fields[i] = None
         print(fields)
         newWine = Wine(
-            
             country_id=fields['country_id'],
             region_id=fields['region_id'],
             name=fields['name'],
@@ -194,11 +230,16 @@ def deleteWine():
     """Delete a wine"""
     if request.method == 'POST':
         wineid = request.form['wineid']
+        # print("WINEID = %s" % wineid)
         session = DBSession()
         wineToDelete = session.query(Wine).filter_by(id=wineid).one_or_none()
+        # print("QUERY COMPLETED WITH: %s" % wineToDelete)
         session.delete(wineToDelete)
+        # print("DELETE CALLED. DESCRIPTION: %s" % wineToDelete.description)
         session.commit()
+        # print("DELETE COMMITTED NOW")
         session.close()
+        # print("DONE WITH DB SESSION")
         return redirect(url_for('showWineTable'))
     else:
         return render_template('wineTable.html')
