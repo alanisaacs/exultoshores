7f3d07f1cd97efabb9fd5643157a2a95a6ebf7ea
Alan Isaacs <xtrasenz@gmail.com>
2021-02-20 16:26:37 -0800

    Move footer definition to shared js file

---
+++ static/scripts_all.js
@@ -0,0 +1,5 @@
+function show_footer() {
+	let	HTMLString = '<p>"I sound my barbaric yawp over the roofs of the world." &mdash;Whitman</p><p>Copyright&copy;2021 exultoshores.com</p>';
+    document.getElementById("main_footer").innerHTML = HTMLString;
+}
+
