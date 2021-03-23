3711ffc45fbd7ea79611c0ea73f015f53cfd8158
Alan Isaacs <xtrasenz@gmail.com>
2021-03-22 18:42:22 -0700

    Add initial table sorting function to shared js

--- static/scripts_all.js
+++ static/scripts_all.js
@@ -3,3 +3,58 @@ function show_footer() {
     document.getElementById("main_footer").innerHTML = HTMLString;
 }
 
+function sort_table() {
+    let asc = true;         // Start ascending as direction
+    let last_col_id = -1;   // Track last column clicked
+    // Select all the TH elements in the document and listen for clicks on them
+    document.querySelectorAll('th').forEach(th => 
+        th.addEventListener('click', () => {
+            // Click is detected
+            // Work on the table element whose header was clicked
+            const table = th.closest('table');
+            // Make an array containing all the rows except the first (header) row
+            let trs = Array.from(table.querySelectorAll('tr:nth-child(n+2)'));
+            // Get the values in each row of the selected column
+            // tracking the original row number as well
+            // in a list of pairs: [[val, row], [val, row], ... ]
+            let col_id = Array.from(th.parentNode.children).indexOf(th);
+            let valrows = [];
+            trs.forEach(tr => {
+                let a = [];
+                a.push(tr.children[col_id].innerText);   //val of cell
+                a.push(tr.rowIndex-1);  //rowIndex starts at 1 not 0
+                valrows.push(a);
+            });
+            // If clicking same column as last time, reverse direction
+            if (col_id == last_col_id) { 
+                asc = !asc;
+            } else {
+                // If clicking on new column, direction is ascending
+                asc = true;
+            };
+            last_col_id = col_id;
+            // Sort valrows by val depending on direction
+            if (asc) {
+                valrows = valrows.sort();
+            } else {
+                //valrows = valrows.reverse();
+                //may fail so use compare function instead
+                valrows.sort((a,b) => {
+                    if (a < b) {
+                        return 1;
+                    } else if (a > b) {
+                        return -1;
+                    return 0;
+                    }
+                })
+            }
+            // Create a list of rows in sorted order by rowIndex
+            let newtrs = [];
+            valrows.forEach(vr => {
+                newtrs.push(trs[vr[1]]);
+            })
+            // Add sorted rows to table, replacing unsorted
+            newtrs.forEach(tr => table.appendChild(tr));
+        })
+    ) 
+}
